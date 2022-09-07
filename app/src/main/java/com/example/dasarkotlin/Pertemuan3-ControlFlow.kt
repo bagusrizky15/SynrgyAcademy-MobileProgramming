6ec804caa8fcd58176e5ec80ea0566a402c7cd45
Bagus Rizky M <mohamad.15@students.amikom.ac.id>
2022-09-07 22:11:28 +0700

    Update Pertemuan3-ControlFlow.kt

--- app/src/main/java/com/example/dasarkotlin/Pertemuan3-ControlFlow.kt
+++ app/src/main/java/com/example/dasarkotlin/Pertemuan3-ControlFlow.kt
@@ -2,61 +2,84 @@ package com.example.dasarkotlin
 
 fun main() {
 
-    //Recal Materi kemarin
-    var arrayList = mutableListOf("Bagus", 1)
-    println(arrayList)
-    arrayList.add("Rizky")
-    println(arrayList)
-    arrayList.removeAt(1)
-    println(arrayList)
-    arrayList.set(1,"Riska")
-    println(arrayList)
-    arrayList.add(1,"WKWK")
-    println(arrayList)
+//    //Recal Materi kemarin
+//    var arrayList = mutableListOf("Bagus", 1)
+//    println(arrayList)
+//    arrayList.add("Rizky")
+//    println(arrayList)
+//    arrayList.removeAt(1)
+//    println(arrayList)
+//    arrayList.set(1,"Riska")
+//    println(arrayList)
+//    arrayList.add(1,"WKWK")
+//    println(arrayList)
+//
+//    //Control Flow
+//    var a = 2
+//    var b = 2
+//    if (a==b)
+//        println("benar")
+//    else
+//        println("salah")
+//
+//    //komparasi nilai
+//    val int1 = 10
+//    val int2 = "10"
+//
+//    println(int1 == int2.toInt())
+//    println(int1.equals(int2))
+//
+//    //fungsi when
+//    val c = when(b) {
+//        2 -> "benar"
+//        else -> "salah"
+//    }
+//    println(c)
+//
+//    //looping
+//    val range = 0..5
+//    for (i in range){
+//        println(i)
+//    }
+//
+//    var items = listOf<String>("Bagus", "Rizky")
+//        for (item in items)
+//            println(item)
+//
+//    //ngeloop indexnya
+//    for (objItem in items.indices)
+//        println(objItem)
+//
+//    println("=====")
+//
+//    //ngeloop isi indexnya
+//    for (objItem in items.indices)
+//        println(items[0])
+//
+//    val size = items.size
+//    println(size)
+//
+//    for (arrItems in items) {
+//        if (arrItems == "Rizky") {
+//            return
+//        }
+//        println(arrItems)
+//    }
 
-    //Control Flow
-    var a = 2
-    var b = 2
-    if (a==b)
-        println("benar")
-    else
-        println("salah")
+    //Hands on Time
+    val tinggiBadan = 1.69
+    val beratBadan = 70
+    val bmi = beratBadan /  (tinggiBadan *  tinggiBadan)
+    println(bmi)
 
-    //komparasi nilai
-    val int1 = 10
-    val int2 = "10"
-
-    println(int1 == int2.toInt())
-    println(int1.equals(int2))
-
-    //fungsi when
-    val c = when(b) {
-        2 -> "benar"
-        else -> "salah"
+    val cekBMI = when(bmi) {
+         in 0.0..18.5 -> "Kurang"
+         in 18.5..22.9 ->  "Normal"
+         in 23.0..24.9 -> "Kelebihan"
+         in 25.0..29.9 -> "Obesitas tk 1"
+         else -> "Ob tk 2"
     }
-    println(c)
+    println(cekBMI)
 
-    //looping
-    val range = 0..5
-    for (i in range){
-        println(i)
-    }
-
-    var items = listOf<String>("Bagus", "Rizky")
-        for (item in items)
-            println(item)
-
-    //ngeloop indexnya
-    for (objItem in items.indices)
-        println(objItem)
-
-    println("=====")
-    //ngeloop indexnya
-    for (objItem in items.indices)
-        println(items[0])
-
-    val size = items.size
-    println(size)
-
-    }
+}
 
