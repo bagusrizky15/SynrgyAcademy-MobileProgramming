0ca7c5fd595211d4e6a222539ff91564925d7666
Bagus Rizky M <mohamad.15@students.amikom.ac.id>
2022-09-08 22:10:26 +0700

    Update Pertemuan4.kt

--- app/src/main/java/com/example/dasarkotlin/Pertemuan4.kt
+++ app/src/main/java/com/example/dasarkotlin/Pertemuan4.kt
@@ -20,9 +20,14 @@ fun main(){
 
     //Looping
 
-    var i = 0
-    while (i<=5)
-        println(i++)
+//    var i = 0
+//    while (i<=5)
+//        println(++i)
+//
+//    println()
+//    var j = 0
+//    while (j<=5)
+//        println(j++)
 
 //    val rows = 5
 //    var k = 0
@@ -41,4 +46,29 @@ fun main(){
 //        k = 0
 //
 //    }
+
+//    for (i in 0..4) {
+//
+//        for (spc in 0..i){
+//            print(" ")
+//        }
+//
+//        for (j in 0..4) {
+//            print("*")
+//        }
+//        println()
+//
+//    }
+
+    for (i in 0..4) {
+        for (spc in i..4){
+            print(" ")
+        }
+        for (j in 1..i) {
+            print("*")
+        }
+        println()
+    }
+
+
 }
