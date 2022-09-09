f065f6971ec993b09f2b9cd98a57ec4cf32a08f1
Bagus Rizky M <mohamad.15@students.amikom.ac.id>
2022-09-09 20:26:33 +0700

    Update Pertemuan5.kt

--- app/src/main/java/com/example/dasarkotlin/Pertemuan5.kt
+++ app/src/main/java/com/example/dasarkotlin/Pertemuan5.kt
@@ -2,7 +2,19 @@ package com.example.dasarkotlin
 
 class vehicle{
 
+    var name: String? = null
+    var model: String? = null
+    var typeFuel: String? = null
 
+    constructor(name: String?, model: String?=null, _typeFuel:String?=null){
+        this.name = name
+        this.model = model
+        typeFuel = _typeFuel
+    }
+}
 
-
+fun main(){
+    val car = vehicle(name = "Johnson")
+//    car.name = "Avanza"
+    print(car.name)
 }
