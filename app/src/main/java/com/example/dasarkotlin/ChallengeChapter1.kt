b8553c499c5dfc9cf5c38528c83521370c2f461c
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2022-09-15 17:38:29 +0700

    Update ChallengeChapter1.kt

--- app/src/main/java/com/example/dasarkotlin/ChallengeChapter1.kt
+++ app/src/main/java/com/example/dasarkotlin/ChallengeChapter1.kt
@@ -36,28 +36,27 @@ fun piramidaTerbalikBintang(){
 
 fun layangBintang(){
     var n = 8
-    for(i in 1 until n){
-        for(j in 1 until n-i){
-            print(" ")
-        }
-        for(k in 1 until i){
-            print("* ")
-        }
-        println("")
-    }
-
+//    for(i in 0 until n){
+//        for(j in 0 until n-i){
+//            print(" ")
+//        }
+//        for(k in 0 until i){
+//            print("* ")
+//        }
+//        println("")
+//    }
     for (i in n downTo 1) {
 
         for (k in 1..n - i) {
-            print(" ")
+            print("xd")
         }
 
         for (j in i..2 * i - 1) {
-            print("* ")
+            print("*=")
         }
 
         for (j in 0..i - 1 - 1) {
-            print("* ")
+            print("ab")
         }
 
         println()
