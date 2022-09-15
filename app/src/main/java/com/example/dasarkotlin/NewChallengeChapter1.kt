b4c92b7f8bac29ceb0bbd7248a5e9bff649c04cf
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2022-09-15 20:08:00 +0700

    Create NewChallengeChapter1.kt

---
+++ app/src/main/java/com/example/dasarkotlin/NewChallengeChapter1.kt
@@ -0,0 +1,17 @@
+package com.example.dasarkotlin
+
+fun ArrayChallenge(arr: Array<Int>): Int {
+
+    // write your code here
+
+}
+
+fun main() {
+    println(ArrayChallenge(readLine()))
+}
+
+//input 1
+fun readLine1(): Array<Int> = arrayOf(44, 30, 24, 32, 35, 30, 40, 38, 15)
+
+//input 2
+fun readLine2(): Array<Int> = arrayOf(10, 9, 8, 2)
