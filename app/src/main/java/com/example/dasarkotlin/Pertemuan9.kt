f74ee2bb26462ca6a2439b8c728f9c39089d6e9a
Bagus Rizky M <mohamad.15@students.amikom.ac.id>
2022-09-21 22:14:09 +0700

    Update Pertemuan9.kt

--- app/src/main/java/com/example/dasarkotlin/Pertemuan9.kt
+++ app/src/main/java/com/example/dasarkotlin/Pertemuan9.kt
@@ -1,2 +1,21 @@
 package com.example.dasarkotlin
 
+abstract class Shape{
+    abstract fun area(): Int
+}
+
+class Rectangle(var width: Int, var height: Int): Shape(){
+    override fun area(): Int {
+        return width*height
+    }
+}
+
+class Square(var edge: Int): Shape(){
+    override fun area(): Int {
+        return edge*edge
+    }
+}
+
+fun main(){
+
+}
