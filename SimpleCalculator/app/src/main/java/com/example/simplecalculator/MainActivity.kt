c2338b78f580e02d2fae569ec702b3654a1a8940
Bagus Rizky M <mohamad.15@students.amikom.ac.id>
2022-09-30 20:16:20 +0700

    Update MainActivity.kt

--- SimpleCalculator/app/src/main/java/com/example/simplecalculator/MainActivity.kt
+++ SimpleCalculator/app/src/main/java/com/example/simplecalculator/MainActivity.kt
@@ -3,16 +3,20 @@ package com.example.simplecalculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.RadioGroup
+import android.widget.Switch
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var costService: TextView
     private lateinit var totalResult: TextView
+    private lateinit var rb: RadioGroup
+    private lateinit var tip: Switch
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        
     }
 }
 
