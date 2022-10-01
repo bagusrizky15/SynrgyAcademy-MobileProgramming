24a66b626de8b61befccacffba6053101fac1a86
Bagus Rizky M <mohamad.15@students.amikom.ac.id>
2022-10-01 17:44:00 +0700

    Update DBHandler.java

--- JMPjava/app/src/main/java/com/example/jmpjava/DBHandler.java
+++ JMPjava/app/src/main/java/com/example/jmpjava/DBHandler.java
@@ -1,9 +1,9 @@
 package com.example.jmpjava;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.util.Log;
 
 public class DBHandler extends SQLiteOpenHelper {
     // creating a constant variables for our database.
@@ -20,16 +20,22 @@ public class DBHandler extends SQLiteOpenHelper {
     private static final String ID_COL = "id";
 
     // below variable is for our course name column
-    private static final String NAME_COL = "nama";
+    private static final String NAMA_COL = "nama";
 
     // below variable id for our course duration column.
-    private static final String ADDRESS_COL = "duration";
+    private static final String ALAMAT_COL = "alamat";
 
     // below variable for our course description column.
-    private static final String DESCRIPTION_COL = "description";
+    private static final String NOMOR_COL = "nomor";
 
     // below variable is for our course tracks column.
-    private static final String TRACKS_COL = "tracks";
+    private static final String GENDER_COL = "gender";
+
+    // below variable is for our course tracks column.
+    private static final String LOKASI_COL = "lokasi";
+
+    // below variable is for our course tracks column.
+    private static final String GAMBAR_COL = "gambar";
 
     // creating a constructor for our database handler.
     public DBHandler(Context context) {
@@ -45,10 +51,12 @@ public class DBHandler extends SQLiteOpenHelper {
         // along with their data types.
         String query = "CREATE TABLE " + TABLE_NAME + " ("
                 + ID_COL + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-                + NAME_COL + " TEXT,"
-                + DURATION_COL + " TEXT,"
-                + DESCRIPTION_COL + " TEXT,"
-                + TRACKS_COL + " TEXT)";
+                + NAMA_COL + " TEXT,"
+                + ALAMAT_COL + " TEXT,"
+                + NOMOR_COL + " TEXT,"
+                + LOKASI_COL + " TEXT,"
+                + GAMBAR_COL + " TEXT,"
+                + GENDER_COL + " TEXT)";
 
         // at last we are calling a exec sql
         // method to execute above sql query
@@ -56,7 +64,7 @@ public class DBHandler extends SQLiteOpenHelper {
     }
 
     // this method is use to add new course to our sqlite database.
-    public void addNewCourse(String courseName, String courseDuration, String courseDescription, String courseTracks) {
+    public void addNewCourse(String namaUser, String alamatUser, String nomorUser, String genderUser, String lokasiUser, String gambarUser) {
 
         // on below line we are creating a variable for
         // our sqlite database and calling writable method
@@ -69,10 +77,12 @@ public class DBHandler extends SQLiteOpenHelper {
 
         // on below line we are passing all values
         // along with its key and value pair.
-        values.put(NAME_COL, courseName);
-        values.put(DURATION_COL, courseDuration);
-        values.put(DESCRIPTION_COL, courseDescription);
-        values.put(TRACKS_COL, courseTracks);
+        values.put(NAMA_COL, namaUser);
+        values.put(ALAMAT_COL, alamatUser);
+        values.put(NOMOR_COL, nomorUser);
+        values.put(GENDER_COL, genderUser);
+        values.put(LOKASI_COL, lokasiUser);
+        values.put(GAMBAR_COL, gambarUser);
 
         // after adding all values we are passing
         // content values to our table.
