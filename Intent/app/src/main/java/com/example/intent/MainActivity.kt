de676f383c34c72ab929a95c43c5847d25ca9853
Bagus Rizky M <mohamad.15@students.amikom.ac.id>
2022-10-05 23:02:49 +0700

    Update MainActivity.kt

--- Intent/app/src/main/java/com/example/intent/MainActivity.kt
+++ Intent/app/src/main/java/com/example/intent/MainActivity.kt
@@ -31,7 +31,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun startActivityForResult(requestCode: Int, resultCode:Int, data:Intent) {
-        
+
     }
 
     companion object {
