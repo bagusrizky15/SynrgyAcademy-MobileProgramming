2659f6411abae541e8fe2a7fb70e4898f343a434
Bagus Rizky M <mohamad.15@students.amikom.ac.id>
2022-10-07 21:09:59 +0700

    button nav

--- Intent/app/src/main/java/com/example/intent/MainFragment.kt
+++ Intent/app/src/main/java/com/example/intent/MainFragment.kt
@@ -2,10 +2,35 @@ package com.example.intent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main_fragment.*
 
 class MainFragment : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_fragment)
+
+        setupBottoomNav()
+    }
+
+    private fun setupBottoomNav() {
+        bottomNav.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.navigation_satu -> {
+                    setupFragment(Fragment1())
+                    true
+                }
+                R.id.navigation_dua -> {
+                    setupFragment(Fragment1())
+                    true
+                }
+                else -> throw Exception("Error")
+            }
+        }
+    }
+
+    private fun setupFragment(fragment1: Fragment1) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment1, fragment1)
+        transaction.commit()
     }
 }
