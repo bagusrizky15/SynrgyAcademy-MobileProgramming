6eb75224075d891695a14aa1b55c93f23e69a551
Bagus Rizky M <mohamad.15@students.amikom.ac.id>
2022-10-08 12:16:32 +0700

    .

--- SimpleCalculator/app/src/main/java/com/example/simplecalculator/MainActivity.kt
+++ SimpleCalculator/app/src/main/java/com/example/simplecalculator/MainActivity.kt
@@ -1,38 +1,34 @@
 package com.example.simplecalculator
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.*
-import androidx.appcompat.widget.SwitchCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var costService: TextView
-    private lateinit var rb: RadioGroup
-    private lateinit var tip: SwitchCompat
-    private lateinit var totalResult: TextView
-    private lateinit var buttonCalculate: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        costService = findViewById(R.id.cost_of_service)
-        rb = findViewById(R.id.radioGroup)
-        tip = findViewById(R.id.round_up_switch)
-        totalResult = findViewById(R.id.tvResult)
-        buttonCalculate = findViewById(R.id.calculate_button)
-
-
-        buttonCalculate.setOnClickListener{
-            val selectedRadioButton: Int = rb.checkedRadioButtonId
-            if (selectedRadioButton != -1)
-            {
-                Toast.makeText(applicationContext, "Dipilih", Toast.LENGTH_SHORT).show()
-            }else {
-                Toast.makeText(applicationContext, "Tidak dipilih", Toast.LENGTH_SHORT).show()
-            }
+        btnCalculate.setOnClickListener(){
+           Intent(this, MainActivity::class.java).also {
+               sendData()
+               getData()
+               startActivity(intent)
+           }
         }
+    }
+
+    private fun getData() {
+        val iniData = intent.getStringExtra("input")
+        tvResult.setText(iniData)
+
+    }
+
+    private fun sendData() {
+        val iniData = etCost.text.toString()
+        intent.putExtra("input", iniData)
     }
 }
 
