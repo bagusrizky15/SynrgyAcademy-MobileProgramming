8c03ce8f891341a21c4f1820d2c4bcfb4e29fd73
Bagus Rizky M <mohamad.15@students.amikom.ac.id>
2022-10-09 22:50:20 +0700

    .

---
+++ ChallengeChapter3/app/src/main/java/com/example/challengechapter3/AdapterList.kt
@@ -0,0 +1,32 @@
+package com.example.challengechapter3
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.annotation.NonNull
+import androidx.recyclerview.widget.RecyclerView
+import com.example.challengechapter3.AdapterList.MyViewHolder
+
+internal class AdapterList(private var itemsList: List<String>) :
+        RecyclerView.Adapter<MyViewHolder>() {
+            internal inner class MyViewHolder(view: View) : RecyclerView.ViewHolder(view){
+                var itemTextView: TextView = view.findViewById(R.id.itemTextView)
+            }
+
+    @NonNull
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val itemView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item, parent, false)
+        return MyViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val item = itemsList[position]
+        holder.itemTextView.text = item
+    }
+
+    override fun getItemCount(): Int {
+        return itemsList.size
+    }
+}
--- ChallengeChapter3/app/src/main/java/com/example/challengechapter3/MainActivity.kt
+++ ChallengeChapter3/app/src/main/java/com/example/challengechapter3/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.challengechapter3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private val itemsList = ArrayList<String>()
+    private lateinit var adapterList: AdapterList
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+        adapterList = AdapterList(itemsList)
+        val layoutManager = LinearLayoutManager(applicationContext)
+        recyclerView.layoutManager = layoutManager
+        recyclerView.adapter = adapterList
+        showList()
     }
+
+    private fun showList() {
+        for (i in 'A'..'Z')
+        {
+            itemsList.add("$i")
+        }
+    }
+
+
 }
