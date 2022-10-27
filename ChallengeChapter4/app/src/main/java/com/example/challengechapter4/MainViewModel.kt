a55cc5e6ca799e22e32cbc590fa087d16db74689
Bagus Rizky M <mohamad.15@students.amikom.ac.id>
2022-10-27 21:59:42 +0700

    .

--- ChallengeChapter4/app/src/main/java/com/example/challengechapter4/MainViewModel.kt
+++ ChallengeChapter4/app/src/main/java/com/example/challengechapter4/MainViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.challengechapter4
 
-class MainViewModel {
+import androidx.lifecycle.MutableLiveData
 
+class MainViewModel {
+    val counter: MutableLiveData<Int> = MutableLiveData(0)
 }
--- ChallengeChapter4/app/src/main/java/com/example/challengechapter4/RecyclerViewAdapter.kt
+++ ChallengeChapter4/app/src/main/java/com/example/challengechapter4/RecyclerViewAdapter.kt
@@ -1,23 +1,36 @@
 package com.example.challengechapter4
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.challengechapter4.databinding.ActivityMainBinding
+import com.example.challengechapter4.databinding.ItemMenuBinding
 
 class RecyclerViewAdapter : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>(){
-    class ViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
-
+    class ViewHolder(val itemBinding: ItemMenuBinding) :
+        RecyclerView.ViewHolder(itemBinding.root) {
+        fun bindItem(
+            item: String
+        ){
+            itemBinding.textMenu.text = item
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val binding = 
+        val binding = ItemMenuBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        when (holder){
+            is ViewHolder -> {
+                holder.bindItem(itemList[position])
+            }
+        }
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return itemList.size
     }
 }
