a86a7dbec147eb607e71055df36ad6705fed41b5
Bagus Rizky M <mohamad.15@students.amikom.ac.id>
2022-10-27 21:16:20 +0700

    .

---
+++ ChallengeChapter4/app/src/main/java/com/example/challengechapter4/RecyclerViewAdapter.kt
@@ -0,0 +1,23 @@
+package com.example.challengechapter4
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class RecyclerViewAdapter : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>(){
+    class ViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = 
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
--- ChallengeChapter4/app/src/main/java/com/example/challengechapter4/SampleData.kt
+++ ChallengeChapter4/app/src/main/java/com/example/challengechapter4/SampleData.kt
@@ -1,5 +1,12 @@
 package com.example.challengechapter4
 
 object SampleData {
-    
+    val menus: List<String>
+        get() = mutableListOf(
+            "Nasi Uduk",
+            "Nasi Pecel",
+            "Ayam Goreng",
+            "Nasi Goreng",
+            "Mie Goreng"
+        )
 }
