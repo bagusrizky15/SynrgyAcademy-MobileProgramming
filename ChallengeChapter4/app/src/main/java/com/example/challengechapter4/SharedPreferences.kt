5a4410ff2a2f40cc8ea148c2106cf3e33243efb6
Bagus Rizky M <mohamad.15@students.amikom.ac.id>
2022-10-28 19:57:41 +0700

    Create SharedPreferences.kt

---
+++ ChallengeChapter4/app/src/main/java/com/example/challengechapter4/SharedPreferences.kt
@@ -0,0 +1,20 @@
+package com.example.challengechapter4
+
+import android.content.Context
+import android.content.Context.MODE_PRIVATE
+import androidx.core.content.edit
+
+class SharedPreferences(context: Context) {
+    private val sharedPreferencesName = "SharedPreferencesBinar"
+    private var preferences = context.getSharedPreferences(sharedPreferencesName, 0)
+
+    var isLogin : Boolean
+        set(value) = {
+            preferences.edit{
+                putBoolean("IS_LOGIN", value)
+            }
+        }
+
+    get() = preferences.getBoolean("IS_LOGIN", false)
+
+}
