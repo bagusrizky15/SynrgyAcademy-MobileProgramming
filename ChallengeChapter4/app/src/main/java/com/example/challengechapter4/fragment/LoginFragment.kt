e726e72874b0f8a5bda2e5650b30a1cf2b66447f
Bagus Rizky M <mohamad.15@students.amikom.ac.id>
2022-11-02 23:03:49 +0700

    Update LoginFragment.kt

--- ChallengeChapter4/app/src/main/java/com/example/challengechapter4/fragment/LoginFragment.kt
+++ ChallengeChapter4/app/src/main/java/com/example/challengechapter4/fragment/LoginFragment.kt
@@ -1,17 +1,19 @@
 package com.example.challengechapter4.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.example.challengechapter4.R
 import com.example.challengechapter4.helper.PrefHelper
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
 
     private lateinit var preferences: PrefHelper
-
+    private lateinit var btnLogin: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -25,7 +27,11 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
-        
+
+        btnLogin.setOnClickListener{
+            preferences.getBoolean(false)
+        }
+
     }
 
     companion object {
