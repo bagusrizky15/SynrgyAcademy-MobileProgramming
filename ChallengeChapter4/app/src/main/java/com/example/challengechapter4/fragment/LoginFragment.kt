b933e29852e748723cd07c32b2e6ada41aca72df
Bagus Rizky M <mohamad.15@students.amikom.ac.id>
2022-11-03 19:49:18 +0700

    .

--- ChallengeChapter4/app/src/main/java/com/example/challengechapter4/fragment/LoginFragment.kt
+++ ChallengeChapter4/app/src/main/java/com/example/challengechapter4/fragment/LoginFragment.kt
@@ -6,8 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import com.example.challengechapter4.R
+import com.example.challengechapter4.helper.Constant
 import com.example.challengechapter4.helper.PrefHelper
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
@@ -15,8 +17,6 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
     private lateinit var preferences: PrefHelper
     private lateinit var btnLogin: Button
     private lateinit var tvRegister: TextView
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -30,9 +30,12 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_login, container, false)
+        var editUsername : EditText = view.findViewById(R.id.etUsername)
+        var editPassword : EditText = view.findViewById(R.id.etPassword)
         tvRegister = view.findViewById(R.id.btnRegister)
         btnLogin = view.findViewById(R.id.btnLogin)
         btnLogin.setOnClickListener {
+            preferences.put(Constant.PREF_IS_USERNAME, editUsername.text.toString())
             val fragment = HomeFragment()
             val transaction = fragmentManager?.beginTransaction()
             transaction?.replace(R.id.fragmentContainer, fragment)?.commit()
@@ -45,6 +48,15 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
         }
 
         return view
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (preferences.getBoolean(Constant.PREF_IS_LOGIN)){
+            val fragment = HomeFragment()
+            val transaction = fragmentManager?.beginTransaction()
+            transaction?.replace(R.id.fragmentContainer, fragment)?.commit()
+        }
     }
 
     companion object {
--- ChallengeChapter4/app/src/main/java/com/example/challengechapter4/model/NoteViewModel.kt
+++ ChallengeChapter4/app/src/main/java/com/example/challengechapter4/model/NoteViewModel.kt
@@ -1,4 +1,5 @@
 package com.example.challengechapter4.model
 
 class NoteViewModel {
+
 }
