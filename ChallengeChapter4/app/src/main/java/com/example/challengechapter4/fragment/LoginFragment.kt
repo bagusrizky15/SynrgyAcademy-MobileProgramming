94ffa264a631621d1b5b03fef8c25694f84f565d
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2022-11-03 17:25:25 +0700

    add login

--- ChallengeChapter4/app/src/main/java/com/example/challengechapter4/fragment/LoginFragment.kt
+++ ChallengeChapter4/app/src/main/java/com/example/challengechapter4/fragment/LoginFragment.kt
@@ -6,16 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.EditText
 import com.example.challengechapter4.R
-import com.example.challengechapter4.helper.Constant
 import com.example.challengechapter4.helper.PrefHelper
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
 
     private lateinit var preferences: PrefHelper
     private lateinit var btnLogin: Button
-    lateinit var username: EditText
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -28,15 +27,15 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
-
-        btnLogin.setOnClickListener{
-            if (username.text.isNotEmpty()){
-                preferences.put(Constant.PREF_IS_USERNAME, username.text.toString())
-                preferences.put(Constant)
-            }
+        val view = inflater.inflate(R.layout.fragment_login, container, false)
+        btnLogin = view.findViewById(R.id.btnLogin)
+        btnLogin.setOnClickListener {
+            val fragment = HomeFragment()
+            val transaction = fragmentManager?.beginTransaction()
+            transaction?.replace(R.id.fragmentContainer, fragment)?.commit()
         }
 
+        return view
     }
 
     companion object {
